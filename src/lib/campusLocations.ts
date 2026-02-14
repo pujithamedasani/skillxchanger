90c3e94f86692294ae3856942965d88f4915415b
Pujitha Medasani <mpujitha676@gmail.com>
2026-02-14 20:11:35 +0530

    loaction

--- src/lib/campusLocations.ts
+++ src/lib/campusLocations.ts
@@ -9,7 +9,7 @@ export const CAMPUS_LOCATIONS = [
   { name: "Sports Ground", lat: 16.4610169, lng:80.5076187},
   { name: "Auditorium", lat: 16.4639750, lng:80.5073243},
   { name: "SR Block", lat: 16.4627374, lng: 80.5066156},
-  { name: "Homi-J Baba Block", lat: 16.4648216, lng:  80.5083040 },
+  { name: "Homi J.Baba Block", lat: 16.4648216, lng:  80.5083040 },
   { name: "CV Block", lat: 16.4618748, lng: 80.5059427 },
 ] as const;
 
